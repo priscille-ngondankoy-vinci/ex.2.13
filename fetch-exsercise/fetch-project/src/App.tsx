e2718a91e1ac2362fcbaf6c319e3bf6187f455a2
Priscille Ngondankoy Nkoy <priscille.ngondankoy@student.vinci.be>
2025-11-20 14:50:23 +0100

    ex.2.14

--- fetch-exsercise/fetch-project/src/App.tsx
+++ fetch-exsercise/fetch-project/src/App.tsx
@@ -1,88 +1,64 @@
 import { useEffect, useState } from 'react'
 
 import './App.css'
-import { Outlet} from "react-router-dom";
-import type { Joke, NewJoke, JokeContext } from "../src/types";
+import type { Joke, NewJoke } from "../src/types";
 
-interface JokeMenuProps {
-  jokes: Joke[];
-}
-const JokeMenu = ({ jokes }: JokeMenuProps) => {
-  return (
-    <div className="pizza-menu">
-    <h2>Nos jokes</h2>
-    <table>
-      <thead>
-        <tr>
-          <th>Joke</th>
-          <th>Contenu</th>
-        </tr>
-      </thead>
-      <tbody>
-        {jokes.map((joke) => (
-          <tr key={joke.id}>
-            <td>{joke.title}</td>
-            <td>{joke.content}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-    </div>
-  );
-};
 
 
 const App = () => {
-  const [actionToBePerformed, setActionToBePerformed] = useState(false);
   const [jokes, setJokes] = useState<Joke[]>([]);
+  const [count, setCount] = useState(0);
+
+
+ 
   const addJoke = (newJoke: NewJoke) => {
     const jokeAdded = { ...newJoke, id: nextJokeId(jokes) };
     setJokes([...jokes, jokeAdded]);
   };
     useEffect(() => {
+      
     fetch("http://localhost:3000/jokes")
       .then((response) => {
         if (!response.ok)
           throw new Error(
             `fetch error : ${response.status} : ${response.statusText}`
           );
+         
         return response.json();
       })
+      
       .then((jokes) => setJokes(jokes))
+      
       .catch((err) => {
         console.error("HomePage::error: ", err);
       });
-  }, []);
-  
-
-  const clearActionToBePerformed = () => {
-    setActionToBePerformed(false);
-  };
-
-  const fullJokeContext: JokeContext = {
-    addJoke,
-    jokes,
-    setJokes,
-    actionToBePerformed,
-    setActionToBePerformed,
-    clearActionToBePerformed,
-    
-  };
-
-
-
-  return (
-    <div className="page">
-     
-      <main>
-        <JokeMenu jokes={jokes} />
-        <Outlet context={fullJokeContext} />
-      </main>
       
-    </div>
-  );
-};
+  const interval = setInterval(() => {
+    setCount((prev) => (prev + 1) % jokes.length);
+  }, 5000); // 10 secondes
+  if (count==10) {
+    setJokes(jokes);
+  }
 
+  return () => clearInterval(interval);
+      
+  }, [jokes]);
+   const currentJoke = jokes[count];
+
+
+
+
+return (
+  <div>
+    {currentJoke && (
+      <div>
+        <h2>{currentJoke.title}</h2>
+        <p>{currentJoke.content}</p>
+      </div>
+    )}
+  </div>
+);
+}
 const nextJokeId = (jokes: Joke[]) => {
   const ids = jokes.map((joke) => joke.id);
   return Math.max(...ids) + 1;
